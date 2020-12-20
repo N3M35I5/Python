4640fbe6ec137d4a3e088de636ef0c10577ed647
N3M35I5 <44340339+N3M35I5@users.noreply.github.com>
2020-12-20 20:28:03 +0530

    Add files via upload
    First Dp coin sorting problem

---
+++ Dynamic Programming/The coin change problem hackerrank.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+#
+
+def printarr(a):
+    for r in a:
+        print(r)
+
+def getWays(n, c):
+    arr=[]
+    for i in range(len(c)):
+        col = []
+        for j in range(n+1):
+            col.append(0)
+        arr.append(col)
+    for i in range(len(c)):
+        arr[i][0]=1
+    for i in range(len(c)):
+        for j in range(n+1):
+            if j < c[i]:
+                arr[i][j]=arr[i-1][j]
+            else:
+                arr[i][j]=arr[i-1][j]+arr[i][j-c[i]]
+    #printarr(arr)
+    return arr[i][j]
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    ways = getWays(n, c)
+    print(ways)
+    # fptr.write(str(ways) + '\n')
+    #
+    # fptr.close()
